eee7a8dc3474082ae0c24e7e55974007ee84ade0
tsyeung <tsyeung@ucdavis.edu>
2025-05-14 22:12:17 -0700

    tiffany analysis + some edits for clarity

--- analysis.R
+++ analysis.R
@@ -1,40 +1,68 @@
-# joint analysis
+#### joint analysis
+### rhesus macaque -- primate lung function##
 
-# Simona ---------------------------
-
-data = read_excel('/Users/simona/Downloads/PFT_demographics.xlsx')
-
-head(data)
-
-str(data)
-summary(data)
-
-nrow(data)
-
-length(unique(data$ID)) 
+## set working directories
+# SIMONA
+# setwd("/Users/simona/Downloads/")
+# SABINE
+# setwd("/Users/sabinehung/sts195/")
+# TIFFANY
+# setwd("~/Downloads/royer_primate_health/")
 
 
-table_IDs <- table(data$ID)
+## loading packages
+library(ggplot2)
+library(readxl)
+library(dplyr)
+library(lattice)
+library(reshape2)
+library(ggcorrplot)
+library(vegan)
+library(colourvalues)
+library(corrr)
+library(FactoMineR)
+library(factoextra)
+library(corrplot)
+library(tourr)
 
+
+
+
+## read in data
+exposure = read_excel("predictor_exposure.xlsx")
+pedigree = read_excel("predictor_pedigree.xlsx")
+demographics = read_excel("PFT_demographics.xlsx")
+biobehavior = read_excel("predictor_biobehavior.xlsx")
+
+
+combined = merge(exposure, demographics, by = c("ID", "StudyID", "Sex"))
+
+
+# ---------- exploratory ----------
+
+dim(demographics)
+str(demographics)
+summary(demographics)
+length(unique(demographics$ID)) 
+table_IDs <- table(demographics$ID)
 duplicate_IDs <- table_IDs[table_IDs > 1]
-
 duplicate_IDs
 
-library(ggplot2)
+dim(exposure)
+str(exposure)
+summary(exposure)
+length(unique(exposure$ID)) 
 
+dim(pedigree)
+str(pedigree)
+summary(pedigree)
+length(unique(pedigree$offspringID))
 
-pred_data = read_excel('/Users/simona/Downloads/predictor_exposure.xlsx')
+dim(combined)
+str(combined)
+summary(combined)
+length(unique(combined$ID)) 
 
-head(pred_data)
-
-str(pred_data)
-summary(pred_data)
-
-nrow(pred_data)
-
-length(unique(pred_data$ID)) 
-
-combined <- merge(data, pred_data, by = "ID")
 
 ggplot(combined, aes(x = Raw_min, y = Raw_max, color = PFTpre_7d_avgPM)) +
   geom_point() +
@@ -44,148 +72,170 @@ ggplot(combined, aes(x = Raw_min, y = Raw_max, color = PFTpre_7d_avgPM)) +
        color = "PFTpre_7d_avgPM") +
   theme_minimal()
 
-ggplot(pred_data, aes(x = StudyID, y = PFTpre_7d_avgPM)) +
+ggplot(exposure, aes(x = StudyID, y = PFTpre_7d_avgPM)) +
   geom_boxplot() +
   labs(title = "Average Air Pollution Exposure 7 Days Before PFT by StudyID", x = "StudyID", y = "PFTpre_7d_avgPM") +
   theme_minimal()
 
+summary(combined)
+
+fivenum(combined$Raw_max)
+fivenum(combined$Raw_min)
+fivenum(combined$PFTpre_7d_avgPM)
+
+plot(combined$Raw_min, combined$Raw_max)
+abline(lm(combined$Raw_max ~ combined$Raw_min))
+
+plot(combined$Raw_min, combined$PFTpre_7d_avgPM)
+
+plot(combined$Raw_max, combined$PFTpre_7d_avgPM)
+
+num_combined = combined %>% 
+  select(where(is.numeric))
+corr_mat <- round(cor(num_combined), 2)
+melted = melt(corr_mat)
+ggplot(melted, aes(x = Var1, y = Var2, fill = value)) +
+  geom_tile()
+
+ggcorrplot(corr_mat)
 
 
-demographic <- read_excel("/Users/simona/Downloads/PFT_demographics.xlsx")
-exposure <- read_excel("/Users/simona/Downloads/predictor_exposure.xlsx")
+# ---------- PCA ----------
 
-combined_df = left_join(demographic, exposure)
+num_combined = combined[sapply(combined, is.numeric)]
+str(num_combined)
 
-y = as.matrix(combined_df[, c("Raw_mean", "Iti_mean", "Cti_mean")])
+# no missing data
+colSums(is.na(num_combined))
+# scale data before PCA -- normalization
+combined_scaled = scale(num_combined)
+dim(combined_scaled)
 
-# 1. Postnatal group without BW_zscore and AgeAtScreen
 
-X_postnatal_1 = select(combined_df, starts_with("postnatal"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
+non_num = combined_scaled[, -which(apply(combined_scaled, 2, var) !=0)]
+ncol(non_num)
+colnames(non_num)
+
+# remove said columns from the data we ultimately do PCA on
+combined_scaled = combined_scaled[, which(apply(combined_scaled, 2, var) !=0)]
+# 218, 154
+dim(combined_scaled)
+
+colnames(combined_scaled)
+
+exposure_PCA = princomp(combined_scaled)
+summary(exposure_PCA)
+
+# loadings tell you how much each variable contributes to a particular principal 
+# component (correlation between the variable and the principal component)
+exposure_PCA$loadings[, 1:10]
+
+
+# plot the scree plot (indicates how much of the data is explained by the first, 
+# second, third, ... principal component
+fviz_screeplot(exposure_PCA, addlabels = TRUE)
+
+#' The first 10 components account for 74.2% of the explained variance.
+
+# principal component analysis results for variables
+var <- get_pca_var(exposure_PCA)
+var
+# coordinates for the variables
+var$coord
+# correlations between variables and dimensions
+var_correlation = data.frame(var$cor)
+
+# cos2 for the variables
+var$cor2
+
+# contributions of the variables
+# provides us with a quantitative measure of the importance of each variable to 
+# our principal components
+var_contributions = data.frame(var$contrib)
+
+fviz_pca_ind(exposure_PCA,
+             col.ind = "cos2", # Color by the quality of representation
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+
+# positive correlated vars point in same direction
+# negative correlated vars point in opposite direction
+fviz_pca_var(exposure_PCA,
+             col.var = "contrib", # Color by contributions to the PC
+             gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+             repel = TRUE     # Avoid text overlapping
+)
+
+
+# plot where points fall on the first two principal components, with arrows representing columns in the original data.
+# biplot(exposure_PCA)
+
+fviz_pca_biplot(exposure_PCA, repel = TRUE, 
+                col.var = "#2E9FDF", # Variables color
+                col.ind = "#696969"  # Individuals color
+)
+
+# Eigenvalues
+eig.val <- get_eigenvalue(exposure_PCA)
+eig.val
+eig.val[1:10,]
+
+# Results for Variables
+res.var <- get_pca_var(exposure_PCA)
+res.var$coord          # Coordinates
+res.var$contrib        # Contributions to the PCs
+res.var$cos2           # Quality of representation 
+
+# Results for individuals
+res.ind <- get_pca_ind(exposure_PCA)
+res.ind$coord          # Coordinates
+res.ind$contrib        # Contributions to the PCs
+res.ind$cos2           # Quality of representation 
+
+
+fviz_contrib(exposure_PCA, choice = 'var', axes = 1)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 2)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 3)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 4)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 5)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 6)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 7)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 8)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 9)
+fviz_contrib(exposure_PCA, choice = 'var', axes = 10)
+
+
+# ----------CCA ----------
+# break exposures into groups
+gest = select(exposure, starts_with("gest"))
+neonatal = select(exposure, starts_with("neonatal"))
+infancy = select(exposure, starts_with("infancy"))
+postnatal = select(exposure, starts_with("postnatal"))
+
+y = as.matrix(combined[, c("Raw_mean", "Iti_mean", "Cti_mean")])
+num_combined = combined[sapply(combined, is.numeric)]
+
+
+# Postnatal group without BW_zscore and AgeAtScreen
+X_postnatal_1 = select(combined, starts_with("postnatal"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
                        -contains("count"))
 X_postnatal_matrix = as.matrix(X_postnatal_1)
 
 res_postnatal_1 <- CCorA(X_postnatal_matrix, y)
 biplot(res_postnatal_1, xlab = '')
 
-# It seems that the iTi_mean and Raw_mean could be a group, as the arrows point in nearly the same direction.
-
-# 2. Postnatal group with BW_zscore and AgeAtScreen
-
-X_postnatal_2 = select(combined_df, BW_zscore, AgeAtScreen, starts_with("postnatal"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
+# Postnatal group with BW_zscore and AgeAtScreen
+X_postnatal_2 = select(combined, BW_zscore, AgeAtScreen, starts_with("postnatal"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
                        -contains("count"))
 X_postnatal_matrix_2 = as.matrix(X_postnatal_2)
 
 res_postnatal_2 <- CCorA(X_postnatal_matrix_2, y)
 biplot(res_postnatal_2, xlab = '')
 
-# Sabine -----------------------------------------------------------
-
-## exploratory analysis
-
-library(openxlsx)
-
-PFT_demographic = read_excel("/Users/sabinehung/sts195/PFT_demographics.xlsx")
-predictor_exposure = read_excel("/Users/sabinehung/sts195/predictor_exposure.xlsx")
-predictor_pedigree = read_excel("/Users/sabinehung/sts195/predictor_pedigree.xlsx")
-predictors_description = read_excel("/Users/sabinehung/sts195/predictors_description.xlsx")
-
-# Interpret the Data
-
-#' IMPORTANT (from Adventures in Data Science, 8. Data Visualization )
-#'    Good Data Visualizations = provide rapid access to data, faithfully
-#'        represent the data and tell a story, expressive, effective
-#'    Bad Data Visualizations = too much or too little information, inconsistent,
-#'        ignore limits of human perception, misrepresent the data, use
-#'        inappropriate or garbage data
-
-#'  BRAINSTORMING
-#'  1. What is the average body weight for males/females?
-#'  2. What is the relationship between 'Raw_std' vs 'Rti_std'?
-
-#' General outline of 'PFT_demographic' data
-#' Working with tidy data?
-#' 1. Each observation has its own row. yes
-#' 2. Each feature has its own column. yes
-#' 3. Each value has its own cell. yes
-#' Things to Note
-#'    PFT = pulmonary function test
-#'    Raw = resistance in the airway
-#'    Rti = resistance of tissues
-#'    Cti = compliance of tissues
-#'    Iti = intertance of tissues
-#'    Eti = mean elastance of tissues
-ncol(PFT_demographic) # number of columns = 37
-nrow(PFT_demographic) # number of rows = 218
-colnames(PFT_demographic)
-summary(PFT_demographic)
-
-# 1. What is the correlation among 'Cti_std' vs 'Rti_std' vs 'Iti_std'?
-cor(PFT_demographic[c("Cti_std", "Rti_std", 'Iti_std')]) 
-#' corr btwn Cti_std vs Rti_std = 0.4153503
-#' corr btwn Cti_std vs Iti_std = 0.1238057
-#' corr btwn Rti_std vs Iti_std = 0.5231102
-
-# 2. What is the correlation among 'Cti_mean' vs 'Rti_mean' vs 'Iti_mean' vs 'Eti'?
-cor(PFT_demographic[c("Cti_mean", "Rti_mean", 'Iti_mean', 'Eti')]) 
-#' corr btwn Cti_mean vs Rti_mean = -0.3091232
-#' corr btwn Cti_mean vs Iti_mean = -0.1076574
-#' corr btwn Cti_mean vs Eti = -0.6813732
-#' corr btwn Rti_mean vs Iti_mean = 0.1111173
-#' corr btwn Rti_mean vs Eti = 0.5033897
-#' corr btwn Iti_mean vs Eti = 0.2127782
-
-# 3. VISUALIZATION: What is the relationship between 'Raw_min' and 'Raw_max'?
-ggplot(PFT_demographic) + aes(x = Raw_min, y = Raw_max) + geom_point()
-
-# General outline of 'predictor_exposure' data
-#' Working with tidy data?
-#' 1. Each observation has its own row. yes
-#' 2. Each feature has its own column. yes
-#' 3. Each value has its own cell. yes
-#' Things to Note
-#'    gestational
-#'    neonatal = newborn, first 30 days of life
-#'    infancy
-#'    postnatal
-ncol(predictor_exposure) # number of columns = 129
-nrow(predictor_exposure) # number of rows = 218
-summary(predictor_exposure)
-
-#' 4. VISUALIZATION: What do the relationships between 'gest_avgPM', 'gest_avgO3'
-#'    'gest_avgNO2' look like?
-library("ggplot2")
-# gest_avgPM vs gest_avgO3
-ggplot(predictor_exposure) + aes(x = gest_avgPM, y = gest_avgO3) + geom_point()
-# gest_avgNO2 vs gest_avgO3
-ggplot(predictor_exposure) + aes(x = gest_avgNO2, y = gest_avgO3) + geom_point()
-# gest_avgNO2 vs gest_avgPM
-ggplot(predictor_exposure) + aes(x = gest_avgNO2, y = gest_avgPM) + geom_point()
-
-# 5. VISUALIZATION: Mass plots on different categories
-plot(predictor_exposure[,1:12]) # gest_nanCount
-plot(predictor_exposure[,22:33]) # neonatal_nanCount
-plot(predictor_exposure[,47:52]) # infancy_count
-
-## cca
-
-# CODE
-# my age group: gestational
-
-# load data sets
-demographic = read_excel("/Users/sabinehung/sts195/PFT_demographics.xlsx")
-exposure = read_excel("/Users/sabinehung/sts195/predictor_exposure.xlsx")
-pedigree = read_excel("/Users/sabinehung/sts195/predictor_pedigree.xlsx")
-
-combined_df = left_join(demographic, exposure)
-
-# load packages
-library(vegan)
-library(colourvalues)
-
-# gestational
-
-y = as.matrix(combined_df[, c("Raw_mean", "Iti_mean", "Cti_mean")])
-X1_df = select(combined_df, starts_with("gest"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
+# Gestational group without BW_zscore and AgeAtScreen
+X1_df = select(combined, starts_with("gest"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
                -contains("count"))
 X1 = as.matrix(X1_df)
 
@@ -193,24 +243,11 @@ res_vegan <- CCorA(X1, y)
 X1_scores <- res_vegan$Cx
 Y1_scores <- res_vegan$Cy
 
-print(res_vegan)
 biplot(res_vegan)
 
-#' INTERPRETATION
-#'    For my age group of choosing, I went with "gestational" which we went over 
-#' a bit during one of our previous meetings. Just by looking at the top two 
-#' graphs, I immediately noticed how Obj67 (from the red chart) and Obj161 (from 
-#' the blue chart) stood out among the rest. To me, this indicates that the two 
-#' are vastly different from the other objects and have little to no correlation.
-#'    As for the second set of charts, I noticed that gest_avgO3 and gest_avgTemp
-#' were closely correlated with one another given how their arrows were near each
-#' other in the third quadrant. My educated guess is that since Iti_mean is directly
-#' between Raw_mean and Cti_mean, it has equal correlation between the two, not
-#' more or less with one or the other.
+# Gestational group with BW_zscore, AgeAtScreen
 
-# gestational (with BW_zscore, AgeAtScreen)
-
-X2_df = select(combined_df, BW_zscore, AgeAtScreen, starts_with("gestational"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
+X2_df = select(combined, BW_zscore, AgeAtScreen, starts_with("gestational"), -contains("totalhours"), -contains("nanCount"), -contains("AUC"),
                -contains("count"))
 X2 = as.matrix(X2_df)
 
@@ -218,25 +255,34 @@ res_vegan_2 <- CCorA(X2, y)
 X2_scores <- res_vegan$Cx
 Y1_scores <- res_vegan$Cy
 
-print(res_vegan_2)
 biplot(res_vegan_2)
 
-#' INTERPRETATION
-#'    For these second set of plots, the difference between these and the first
-#' set is that this now includes BW_zscore and AgeAtScreen. Immediately, I noticed
-#' Obj161 (a bit cut off but is my best guess), Obj161 (again possibly?), and
-#' Obj151 deviated greatly among the rest. Obj161 was particularly far from the
-#' other x values which tells me that this primate's Raw_mean, Iti_mean, and 
-#' Cti_mean were all vastly different from the other primates -- this brings 
-#' up a good question that could lead to some interesting findings with a 
-#' deeper look into things.
-#'    As for the bottom two plots, BW_zscore and AgeAtScreen in the second and
-#' third quadrants displayed a complete 90-degree angle, indicating that there 
-#' is no correlation between the two whatsoever. The Raw_mean and Iti_mean were
-#' fairly close to one another meaning there was correlation, and could tell us
-#' that as one increases so does the other -- so, tissue inertance increases
-#' as resistance of airways increases. Cti_mean and Raw_mean were nearly 
-#' 90-degrees apart from each other so they have essentially no correlation with
-#' each other.
+gest = gest[, which(apply(gest, 2, var) !=0)]
+gest
 
-# Tiffany -----------------------------------------------------------
+res_vegan_1 <- CCorA(gest, y)
+gest_scores <- res_vegan_1$Cx
+y_scores <- res_vegan_1$Cy
+res_vegan_1
+
+
+# neonatal group without BW_zscore and AgeAtScreen
+
+exp = exposure[, which(apply(exposure, 2, var) !=0)] |>
+  # don't take columns with basic counts 
+  select(-contains(c("totalhours", "nanCount", "AUC", "count")))
+
+neonatal = select(exp, starts_with("neonatal"))
+neonatal_cca <- CCorA(neonatal, y)
+neonatal_scores <- neonatal_cca$Cx
+y_scores_2 <- neonatal$Cy
+neonatal_cca
+
+neonatal_rez <- CCorA(y, neonatal)
+biplot(neonatal_rez, xlab = '')
+
+# neonatal group with BW_zscore and AgeAtScreen
+
+neonatal_2 = select(combined, BW_zscore, AgeAtScreen, starts_with("neonatal"), -contains(c("totalhours", "nanCount", "AUC", "count")))
+neonatal_2_rez <- CCorA(y, neonatal_2)
+biplot(neonatal_2_rez, xlab = '')
